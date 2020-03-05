150ea2e5655c357c8868ff61e6c4346c2a969dcf
mirabilos <t.glaser@tarent.de>
2020-03-05 21:03:21 +0100

    update from MirBSD CVS and adjust accordingly

--- mksh/sysadmin/gencert.sh
+++ mksh/sysadmin/gencert.sh
@@ -1,9 +1,11 @@
 #!/bin/mksh
-# $Id: gencert.sh 1505 2010-10-11 12:26:46Z tglase $
-# $MirOS: src/etc/rc,v 1.111 2010/07/11 17:35:09 tg Exp $
+# $Id: gencert.sh 1505+cvs 2010-10-11 12:26:46Z tglase $
+# $MirOS: src/etc/rc,v 1.133 2020/03/05 19:54:37 tg Exp $
 #-
 # Copyright © 2010
 #	mirabilos <t.glaser@tarent.de>
+# Copyright © 2020
+#	mirabilos <m@mirbsd.org>
 #
 # Provided that these terms and disclaimer and all copyright notices
 # are retained or reproduced in an accompanying document, permission
@@ -26,30 +28,42 @@ chain_name=/etc/ssl/deflt-ca.cer
 
 keyperms=640
 keyusers=root:ssl-cert
-#keyperms=600
-#keyusers=0:0
+pubperms=644
+pubusers=0:0
+umask 077
 
-# XXX 6000-8000 is recommended... choose less to be nice to old boxen
 bitsize=4096
 
 if [[ ! -s $seckeyname ]]; then
 	print -n "openssl: generating new host RSA key... "
-	rm -f $seckeyname $pubkeyname $chain_name
-	if openssl genrsa -out $seckeyname $bitsize; then
-		chown $keyusers $seckeyname
-		chmod $keyperms $seckeyname
+	rm -f "$seckeyname" "$pubkeyname" "$chain_name"
+	if x=$(openssl genrsa -out "$seckeyname" "$bitsize" 2>&1); then
+		chown "$keyusers" "$seckeyname"
+		chmod "$keyperms" "$seckeyname"
 		print done.
 	else
-		print failed.
+		rm -f "$seckeyname"
+		print failed:
+		print -r -- "$x" | sed 's/^/| /'
 	fi
+	x=
 fi
-if [[ ! -s $pubkeyname || ! -s $chain_name ]]; then
+[[ -s $pubkeyname && -s $chain_name ]] || \
+    if [[ -s $seckeyname ]]; then
 	print -n "openssl: generating new host X.509v3 certificate... "
-	rm -f $pubkeyname $chain_name
-	openssl req -batch -new -subj "/CN=$(hostname)/" \
-	    -key $seckeyname \
-	    -x509 -out $pubkeyname
-	chmod 644 $pubkeyname
-	cp $pubkeyname $chain_name
-	print done
+	rm -f "$pubkeyname" "$chain_name"
+	if openssl req -batch -new -subj "/CN=$(hostname)/" \
+	    -key "$seckeyname" \
+	    -x509 -out "$pubkeyname"; then
+		cp "$pubkeyname" "$chain_name"
+		chown "$pubusers" "$pubkeyname" "$chain_name"
+		chmod "$pubperms" "$pubkeyname" "$chain_name"
+		print done.
+	else
+		rm -f "$pubkeyname" "$chain_name"
+		print failed!
+	fi
+else
+	rm -f "$pubkeyname" "$chain_name"
+	print "openssl: cannot generate new host X.509v3 certificate: no key"
 fi
