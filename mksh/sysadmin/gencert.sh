308bea568caad0674e907a67252f559a868c473e
mirabilos <m@mirbsd.org>
2024-06-06 02:36:06 +0200

    merge updates from MirBSD CVS

--- mksh/sysadmin/gencert.sh
+++ mksh/sysadmin/gencert.sh
@@ -1,10 +1,10 @@
 #!/bin/mksh
 # $Id: gencert.sh 1505+cvs 2010-10-11 12:26:46Z tglase $
-# $MirOS: src/etc/rc,v 1.136 2021/11/29 20:49:32 tg Exp $
+# $MirOS: src/etc/rc,v 1.139 2024/06/06 00:30:02 tg Exp $
 #-
 # Copyright © 2010
 #	mirabilos <t.glaser@tarent.de>
-# Copyright © 2020
+# Copyright © 2020, 2024
 #	mirabilos <m@mirbsd.org>
 #
 # Provided that these terms and disclaimer and all copyright notices
@@ -23,23 +23,39 @@
 # of said person’s immediate fault when using the work as intended.
 
 export LC_ALL=C.UTF-8
+unset LANGUAGE
 
-seckeyname=/etc/ssl/private/default.key
-pubkeyname=/etc/ssl/default.cer
-chain_name=/etc/ssl/deflt-ca.cer
+pubdirname=/etc/ssl
+pubdirperm=00755
+pubdir_uid=0
+pubdir_gid=0
+chain_name=$pubdirname/deflt-ca.cer
+dhparmname=$pubdirname/dhparams.pem
+pubkeyname=$pubdirname/default.cer
+secdirname=$pubdirname/private
+secdirperm=00710
+secdir_uid=0
+secdir_gid=ssl-cert
+seckeyname=$secdirname/default.key
 
-keyperms=640
-keyusers=root:ssl-cert
-pubperms=644
-pubusers=0:0
+keyperms=0640
+keyusers=$secdir_uid:$secdir_gid
+pubperms=0644
+pubusers=$pubdir_uid:$pubdir_gid
 umask 077
 
-bitsize=4096
+keybits=4096
+dh_bits=2048
+
+[[ -d $pubdirname ]] || install -d \
+    -o "$pubdir_uid" -g "$pubdir_gid" -m "$pubdirperm" "$pubdirname"
+[[ -d $secdirname ]] || install -d \
+    -o "$secdir_uid" -g "$secdir_gid" -m "$secdirperm" "$secdirname"
 
 if [[ ! -s $seckeyname ]]; then
 	print -n "openssl: generating new host RSA key... "
 	rm -f "$seckeyname" "$pubkeyname" "$chain_name"
-	if x=$(openssl genrsa -out "$seckeyname" "$bitsize" 2>&1); then
+	if x=$(openssl genrsa -out "$seckeyname" "$keybits" 2>&1); then
 		chown "$keyusers" "$seckeyname"
 		chmod "$keyperms" "$seckeyname"
 		print done.
@@ -68,4 +84,27 @@ fi
 else
 	rm -f "$pubkeyname" "$chain_name"
 	print "openssl: cannot generate new host X.509v3 certificate: no key"
+fi
+if [[ ! -s $dhparmname ]]; then
+	print "openssl: generating DH parameters"
+	rm -f "$dhparmname"
+	if openssl dhparam -out "$dhparmname" "$dh_bits"; then
+		chown "$pubusers" "$dhparmname"
+		chmod "$pubperms" "$dhparmname"
+		print done.
+	else
+		rm -f "$dhparmname"
+		print failed!
+	fi
+fi
+if [[ -s $pubkeyname && -s $dhparmname ]]; then
+	case $(set +e; grep -Fqxe '-----BEGIN DH PARAMETERS-----' "$pubkeyname"; echo $?) in
+	(0)	;;
+	(1)
+		print openssl: appending DH parameters to certificate for httpd
+		cat "$dhparmname" >>"$pubkeyname"
+		;;
+	(*)	print openssl: error appending DH parameters to certificate
+		;;
+	esac
 fi
